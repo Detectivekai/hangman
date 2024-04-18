4bcd85a24f3bd7cee6a82440b54838ddf590a0dc
student <student@SlovakstudyUbuntu.myguest.virtualbox.org>
2024-04-18 10:02:41 +0200

    hangman

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "hangman.h"
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    srand(time(NULL));
+    char secret[16];
+    get_word(secret);
+    printf("%s\n", secret);
+
+    printf("%d\n", is_word_guessed("secret", "aeiou"));
+    // prints: 0
+    printf("%d\n", is_word_guessed("hello", "aeihoul"));
+    // prints: 1
+
+    char result[30];
+    get_guessed_word("container", "arpstxgoieyu", result);
+    printf("%s\n", result);
+    // result = "_o_tai_er"
+
+
+    
+    return 0;
+}
